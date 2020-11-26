2190381bca8995cb533b515fe8960a8322dae5d0
JahedHossenBangladesh <jahedhossen4358.com>
2020-11-27 00:52:09 +0600

    response setting  res.setHeader,res.write ,res.end

--- first-app.js
+++ first-app.js
@@ -1,5 +1,11 @@
 const http = require('http');
 const server = http.createServer((req,res) =>{
-    console.log(req);
+    console.log(req.url,req.method,req.headers);
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>This is written in Server</title></head>');
+    res.write('<body><h1> Hi , this is written in Server </h1></body>');
+    res.write('</html>');
+    res.end();
 })
 server.listen(3000);
